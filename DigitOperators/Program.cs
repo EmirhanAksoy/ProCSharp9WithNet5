d5214cbcc7f926eedcde0c6f431acb0b9dd43958
EmirhanAksoy <emirhan.aksoy@outlook.com.tr>
2022-01-21 23:43:54 +0300

    Dump method used instead of Console.Writeline

--- DigitOperators/Program.cs
+++ DigitOperators/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using Tutorial.Common.Extensions;
 
-Console.WriteLine($"Integer : {123_465_798 }");
-Console.WriteLine($"Decimal : {123_465_798.098m }");
-Console.WriteLine($"Float : {123_465_798f }");
-Console.WriteLine($"Double : {123_465_798.45 }");
-Console.WriteLine($"Long : {123_465_798_324_565_45L }");
-Console.WriteLine($"Hex : {0x_00_00_0F }");
+$"Integer : {123_465_798 }".Dump();
+$"Decimal : {123_465_798.098m }".Dump();
+$"Float : {123_465_798f }".Dump();
+$"Double : {123_465_798.45 }".Dump();
+$"Long : {123_465_798_324_565_45L }".Dump();
+$"Hex : {0x_00_00_0F }".Dump();
 
 // Binary Literals
 
-Console.WriteLine($"One : {0b_01}");
-Console.WriteLine($"Two : {0b_10}");
-Console.WriteLine($"Three : {0b_11}");
-Console.WriteLine($"Four : {0b_1_00}");
-Console.WriteLine($"Five : {0b_1_01}");
+$"One : {0b_01}".Dump();
+$"Two : {0b_10}".Dump();
+$"Three : {0b_11}".Dump();
+$"Four : {0b_1_00}".Dump();
+$"Five : {0b_1_01}".Dump();
