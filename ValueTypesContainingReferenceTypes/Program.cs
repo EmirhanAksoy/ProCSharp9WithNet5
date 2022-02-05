6f63e6dbfeb85e974e8e28a5ce2c8daf8c118a61
EmirhanAksoy <emirhan.aksoy@outlook.com.tr>
2022-02-05 17:51:26 +0300

    ValueTypesContainingReferenceTypes project was added.

---
+++ ValueTypesContainingReferenceTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tutorial.Common.Extensions;
+
+namespace ValueTypesContainingReferenceTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            "Value Type Containing Reference Types".DumpAsTitle();
+
+
+
+        }
+    }
+
+}
