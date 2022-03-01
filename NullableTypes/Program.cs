6793d80f2a38ca2ed72dc84c58008e40bed0406a
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2022-03-01 23:51:32 +0300

    NullableTypes was explained.

--- NullableTypes/Program.cs
+++ NullableTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tutorial.Common.Extensions;
 
 namespace NullableTypes
 {
@@ -6,7 +7,37 @@ namespace NullableTypes
     {
         static void Main(string[] args)
         {
-           
+            "Nullable Type".DumpAsTitle();
+
+            "Value types can never be assigned the value of null, as that is used to establish an empty object reference.".Dump();
+
+            // Compiler errors!
+            // Value types cannot be set to null!
+            // Uncomment next two lines for checking 
+            // bool myBool = null;
+            // int myInt = null;
+
+            "C# supports the concept of nullable data types. Simply put, a nullable type can represent all the values of its underlying type, plus the value null.".Dump();
+
+            "To define a nullable variable type, the question mark symbol (?) is suffixed to the underlying data type.".Dump();
+
+            "Extremely helpful when working with relational databases, given that it is quite common to encounter undefined columns in database tables".Dump();
+
+            // Define some local nullable variables.
+            int? nullableInt = 10;
+            double? nullableDouble = 3.14;
+            bool? nullableBool = null;
+            char? nullableChar = 'a';
+            int?[] arrayOfNullableInts = new int?[10];
+
+            "The ? suffix notation is a shorthand for creating an instance of the generic System.Nullable<T> structure type.".Dump();
+
+            // Define some local nullable types using Nullable<T>.
+            Nullable<int> _nullableInt = 10;
+            Nullable<double> _nullableDouble = 3.14;
+            Nullable<bool> _nullableBool = null;
+            Nullable<char> _nullableChar = 'a';
+            Nullable<int>[] _arrayOfNullableInts = new Nullable<int>[10];
         }
     }
 }
