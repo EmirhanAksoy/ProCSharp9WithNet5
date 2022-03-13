60ead9fa70aae481f6aeec3027cdc8a81d7fe47f
EmirhanAksoy <emirhan.aksoy@outlook.com.tr>
2022-03-13 17:10:53 +0300

    Constructors project was added.

---
+++ Constructors/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tutorial.Common.Extensions;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            "Constructors".DumpAsTitle();
+        }
+    }
+}
