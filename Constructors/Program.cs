33a4535114160a993d7a605e7367a50c202902b7
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2022-04-05 22:15:42 +0300

    Constructors project updated.

--- Constructors/Program.cs
+++ Constructors/Program.cs
@@ -11,6 +11,7 @@ namespace Constructors
 
             "Every C# class has default constructor".Dump();
 
+
             "Default constructor never takes any argument".Dump();
 
 
@@ -24,6 +25,10 @@ namespace Constructors
 
 
             "We can define custom constructors that takes arguments".Dump();
+
+            "As soon as you define a custom constructor with any number of parameters, the default constructor is silently removed from the class and is no longer available".Dump();
+
+            "To use default constructor, you must explicitly redefine the default".Dump();
 
             "To create multiple custom constructors we can use overloading".Dump();
 
@@ -84,8 +89,36 @@ namespace Constructors
                 this.Name = name;
             }
 
+            //Constructors As Expression-Bodied Members
+            public class Computer
+            {
+                public string RAM { get; set; }
+                public Computer(string ram) => RAM = ram;
+                
+            }
+
+            // Constructors with out Parameters
+            public class Company
+            {
+                public string Name { get; set; }
+
+                public string Address { get; set; }
+
+                public int EmployeeCount { get; set; }
+
+                public Company(string name,string address,int employeeCount,out bool isBigOne)
+                {
+                    Name = name;
+                    Address = address;
+                    EmployeeCount = employeeCount;
+
+                   isBigOne = EmployeeCount > 100;
+                }
+            }
+
 
 
         }
+
     }
 }
